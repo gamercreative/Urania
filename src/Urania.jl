b3df76d05e9cc0e6404520b179387e09afe358c6
akram seifeddine <akramsiefeddine@gmail.com>
2026-08-23 01:25:13 +0300

    fixed the includes

--- src/Urania.jl
+++ src/Urania.jl
@@ -1,4 +1,28 @@
 """
-    The main will include the includes and will be renamed from main.jl to library_name.jl
+    Urania
+
+Machine learning library built from scratch in Julia.
 """
+module Urania
+
 using Statistics
+
+# model base declarations
+include("models/model.jl")
+
+# core
+include("core/activation.jl")
+include("core/loss.jl")
+include("core/optimizer.jl")
+include("core/regularization.jl")
+include("core/training.jl")
+
+# data
+include("data/data_preperation.jl")
+
+
+# model-specific pipelines
+include("models/classic/linear.jl")
+include("models/classic/tree.jl")
+
+end
