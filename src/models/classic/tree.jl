7e5cfe6b952906da968671e84a458dbd5ab8f17a
akram seifeddine <akramsiefeddine@gmail.com>
2026-08-23 13:59:08 +0300

    writing traverse forest function

--- src/models/classic/tree.jl
+++ src/models/classic/tree.jl
@@ -240,13 +240,16 @@ struct RandomForest <: ForestModelType
 end
 
 function create_random_forest(X::AbstractMatrix{<:Real}, y::AbstractVector, tree_count::Int, max_depth::Int, min_sample_split::Int)
+    # used to store the tree types
     rf = TreeModelType[]
-    
+
+    # create the trees one by one and store them
     for _ in 1:tree_count
         X_t, y_t = bootstrap_sampling(X,y)
         push!(rf, build_classifier_tree_model(X_t, y_t, max_depth, min_sample_split))
     end
 
+    # create the random forest and return
     model = RandomForest(
         rf,
         tree_count,
@@ -255,4 +258,8 @@ function create_random_forest(X::AbstractMatrix{<:Real}, y::AbstractVector, tree
     )
 
     return model
+end
+
+function traverse_forest(fores::ForestModelType, x::AbstractMatrix{<:Real})
+    
 end
